e0058e6996f5f7f7cae12d384c748b3bc7dcb159
Joya322 <jannatferdoush1212@gmail.com>
2024-03-16 22:13:58 +0600

    ternary operator added

--- js/phone.js
+++ js/phone.js
@@ -17,7 +17,7 @@ const displayPhones = (phones, isShowAll) => {
   cards.textContent = "";
 
   // show limited phones
-  console.log(phones.length);
+  // console.log(phones.length);
   //get show all button
   const showAllBtn = document.getElementById("show-details");
   if (phones.length > 12 && !isShowAll) {
@@ -46,8 +46,8 @@ const displayPhones = (phones, isShowAll) => {
             <div class="card-body">
               <h2 class="card-title">${phone.phone_name}</h2>
               <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+              <div class="card-actions justify-center">
+                <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
               </div>
             </div>
     `;
@@ -57,6 +57,64 @@ const displayPhones = (phones, isShowAll) => {
   });
   // hide loading spinner
   toggleLoadingSpinner(false);
+};
+
+// handle show details function
+const handleShowDetails = async (id) => {
+  // load single phone data
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  const phoneDetails = data.data;
+  showPhoneDetails(phoneDetails);
+};
+
+// showPhoneDetails
+const showPhoneDetails = (phoneDetails) => {
+  console.log(phoneDetails);
+
+  // get show details container
+  const showDetailsContainer = document.getElementById("showDetailsContainer");
+
+  showDetailsContainer.innerHTML = `
+    <img class="w-2/4 p-4 mb-4 mx-auto" src="${phoneDetails.image}" alt="">
+    <h3 class="font-bold text-lg">${phoneDetails.name}</h3>
+
+    <p><span class="font-bold">Storage:</span> ${
+      phoneDetails.mainFeatures.storage
+    }</p>
+
+    <p><span class="font-bold">Display Size:</span> ${
+      phoneDetails.mainFeatures.displaySize
+    }</p>
+
+    <p><span class="font-bold">Chipset:</span> ${
+      phoneDetails.mainFeatures.chipSet
+    }</p>
+
+    <p><span class="font-bold">Memory:</span> ${
+      phoneDetails.mainFeatures.memory
+    }</p>
+
+    <p><span class="font-bold">Slug:</span> ${phoneDetails.slug}</p>
+
+    <p><span class="font-bold">Release data:</span> ${
+      phoneDetails.releaseDate
+    }</p>
+
+    <p><span class="font-bold">Brand:</span> ${phoneDetails.brand}</p>
+
+    <p><span class="font-bold">GPS:</span> ${
+      phoneDetails?.others?.GPS || "no GPS available"
+    }</p>
+
+    // ternary operator
+    <p><span class="font-bold">GPS:</span> ${
+      phoneDetails?.others?.GPS ? phoneDetails?.others?.GPS : "no GPS available"
+    }</p>
+  `;
+  phone_details_modal.showModal();
 };
 
 // handle search button
